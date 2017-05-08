e3b0ce75350b12a401f2d053dbd042c7958d7b94
Gijs <gijsmw@hotmail.com>
2017-05-08 14:26:54 +0200

    Directory and file setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='UrbanSearch',
+    version='0.1',
+    packages=['urbansearch', 'tests'],
+    url='https://github.com/urbansearchTUD/UrbanSearch',
+    license='GPL-3.0',
+    author='urbanSearchTUD',
+    author_email='',
+    description='',
+    install_requires=[
+        'pytest',
+    ]
+)
+
