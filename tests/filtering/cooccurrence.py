ed4d0afe73561c8dfae19c7d53381efa2a98f2e2
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-05-10 11:21:05 +0200

    Add tests for cooccurrence and fix bugs that were found

--- tests/filtering/cooccurrence.py
+++
@@ -1,6 +0,0 @@
-from urbansearch.filtering import cooccurrence
-
-
-c = cooccurrence.CoOccurrenceChecker()
-
-print(c.check("Amsterdam"))
--- urbansearch/filtering/cooccurrence.py
+++ urbansearch/filtering/cooccurrence.py
@@ -1,4 +1,5 @@
 import ahocorasick
+import collections
 import itertools
 
 from urbansearch.utils import db_utils
@@ -22,8 +23,6 @@ class CoOccurrenceChecker(object):
         [self.automaton.add_word(city, city) for city in cities]
         self.automaton.make_automaton()
 
-        self.occurrences = None
-
     def check(self, page):
         """
         Checks the given page for city co-occurrences and returns them as a list.
@@ -32,34 +31,38 @@ class CoOccurrenceChecker(object):
         :return: The co-occurrences as a list of tuples of city names or None.
         """
         if not isinstance(page, str):
-            raise IOError('Expected a string object but got %s' % type(page))
+            raise TypeError('Expected a string object but got %s' % type(page))
 
         # Retrieve all occurrences of cities, with overlapping occurrences (e.g. Ee and Een) removed.
         # Additionally, duplicate occurrences are removed as the result of clean is a set.
-        self._clean(self.automaton.iter(page))
+        occurrences = self._calculate_occurrences(page)
 
         # if the occurrence list is smaller than 2, there are no co-occurrences and the page may be discared.
-        if len(self.occurrences) < 2:
+        if len(occurrences) < 2:
             return None
 
         # Return a list of tuples, with each tuple being a co-occurrence of two cities.
-        return list(itertools.combinations(self.occurrences, 2))
+        return list(itertools.combinations(occurrences, 2))
 
-    def _clean(self, raw_occurrences):
-        result_set = set()
+    def _calculate_occurrences(self, page):
+        occurrences = self.automaton.iter(page)
+        result_set = collections.OrderedDict()
 
         # Loop over the occurrences pairwise: (1, 2), (2, 3), (3, 4), etc.
-        current = next(raw_occurrences, None)
-        for occurrence in raw_occurrences:
+        prev_occurrence = next(occurrences, None)
+        for occurrence in occurrences:
             # If the end indices of a pair are within the length of an occurrence, there is overlap.
-            if abs(current[0] - occurrence[0]) < len(current[1]):
-                longer = current[1] if len(current[1]) > len(occurrence[1]) else occurrence[1]
-                # On overlap, only add the longer city name to the result set (e.g. discard Ee when Een is encountered)
-                result_set.add(longer)
-            # If there is no overlap, just add the city name to the result set.
+            if abs(occurrence[0] - prev_occurrence[0]) < len(occurrence[1]):
+                longer = occurrence[1] if len(occurrence[1]) > len(prev_occurrence[1]) else prev_occurrence[1]
+                result_set[longer] = None
+                # Skip next to avoid processing the overlap in the next pair
+                prev_occurrence = next(occurrences, None)
+            # If there is no overlap, just add the city names to the result set.
             else:
-                result_set.add(current[1])
+                result_set[prev_occurrence[1]] = None
+                prev_occurrence = occurrence
 
-            current = occurrence
+        if prev_occurrence:
+            result_set[prev_occurrence[1]] = None
 
-        self.occurrences = result_set
+        return result_set.keys()
