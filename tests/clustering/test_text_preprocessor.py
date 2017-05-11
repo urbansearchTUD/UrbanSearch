4e80d86e6858d29ac3084014be49f6fdf5db311f
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-05-11 16:54:33 +0200

    Extend DutchStemmer for better stemming results on verbs

--- tests/clustering/test_text_preprocessor.py
+++ tests/clustering/test_text_preprocessor.py
@@ -1,80 +1,110 @@
 from urbansearch.clustering import text_preprocessor
-from unittest.mock import Mock
 
 p = text_preprocessor.PreProcessor()
-p.stemmer = Mock()
-text_preprocessor.word_tokenize = Mock()
-text_preprocessor.Counter = Mock()
-text_preprocessor.stopwords = Mock()
 
 
 def test_stem():
-    """
-    test the stemming method
-    :return: 
-    """
-    text = ["handel", "bemoederen", "koe"]
-    expected = ["handel", "bemoeder", "koe"]
-    p.stem(text)
-    p.stemmer.stem.assert_called()
+    text = ['handelt', 'bemoederen', 'koe', 'vallen', 'valt', 'gymt']
+    expected = ['handel', 'bemoeder', 'koe', 'val', 'val', 'gym']
+    assert expected == p.stem(text)
 
 
 def test_strip_words():
-    """
-    test the stripping words method
-    :return: 
-    """
-    text_preprocessor.stopwords.words.return_value = ["het"]
-    text = ["verantwoorden", "verantwoorde", "verantwoord"]
-    p.strip_words(text)
-    text_preprocessor.stopwords.words.assert_called()
+    text = ['de', 'buik', 'van', 'Marko']
+    expected = ['buik', 'Marko']
+    assert expected == p.strip_words(text)
 
 
 def test_strip_punctuations():
-    """
-    test the stripping punctuations method
-    :return: 
-    """
-    text = "a, . cd ; f"
-    expected = "a  cd  f"
+    text = 'a, . cd ; f'
+    expected = 'a  cd  f'
     result = p.strip_punctuations(text)
-    assert(result == expected)
+    assert result == expected
 
 
 def test_tokenize():
-    """
-    test the tokenizing method
-    :return: 
-    """
-    text = "een twee drie"
-    expected = ["een", "twee", "drie"]
-    result = p.tokenize(text)
-    text_preprocessor.word_tokenize.assert_called()
+    text = 'een twee drie'
+    expected = ['een', 'twee', 'drie']
+    assert expected == p.tokenize(text)
 
 
 def test_count():
-    """
-    test the counting method
-    :return:
-    """
-    text = ["drie", "drie", "drie", "een", "twee", "twee"]
+    text = ['drie', 'drie', 'drie', 'een', 'twee', 'twee']
     expected = {'Drie': 3, 'Twee': 2, 'Een': 1}
-    result = p.count(text)
-    text_preprocessor.Counter.assert_called()
+    assert expected == p.count(text)
 
 
-def test_pre_process():
-    """
-    test the combined methods
-    :return: 
-    """
-    with open("tests/resources/test.txt", "r") as file_object:
-        text_preprocessor.word_tokenize.return_value = ["a", "b", "c"]
-        text_preprocessor.stopwords.words.return_value = ["het"]
-        text = file_object.read()
-        p.pre_process(text)
+def test_full():
+    text = "Dit is een liedje: een, twee, drie, vier. " \
+           "Hoedje van, hoedje van. Een, twee, drie, vier. Hoeden van papier"
+    expected = {'Hoed': 3, 'Drie': 2, 'Twee': 2,
+                'Vier': 2, 'Papier': 1, 'Lied': 1}
+    assert expected == p.pre_process(text)
 
-        text_preprocessor.word_tokenize.assert_called()
-        p.stemmer.stem.assert_called()
-        text_preprocessor.stopwords.words.assert_called()
-        text_preprocessor.Counter.assert_called()
+
+def test_stem_diminutives_standard():
+    text = ['hoedjes', 'feetje', 'tafeltjes']
+    expected = ['hoed', 'fee', 'tafel']
+    assert expected == p.stem(text)
+
+
+def test_stem_diminutives_exceptions():
+    text = ['welletjes', 'innetjes', 'vieruurtjes']
+    expected = ['welletjes', 'innetjes', 'vieruurtje']
+    assert expected == p.stem(text)
+
+
+def test_stem_diminutives_etjes():
+    text = ['slangetje', 'vinnetje', 'leerlingetje']
+    expected = ['slang', 'vin', 'leerling']
+    assert expected == p.stem(text)
+
+
+def test_stem_diminutives_inkjes():
+    text = ['koninkje', 'plankje', 'vinkje']
+    expected = ['koning', 'plank', 'ving']
+    assert expected == p.stem(text)
+
+
+def test_stem_double_consonents():
+    text = ['hoedden', 'klanten', 'rijdt', 'lessen']
+    expected = ['hoed', 'klant', 'rijd', 'les']
+    assert expected == p.stem(text)
+
+
+def test_stem_double_vowels():
+    text = ['lopen', 'loop', 'keren', 'keer', 'maken', 'maak', 'vuren', 'vuur']
+    # Since its more difficult to correct the
+    # vowels, all double vowels are replaced by singles.
+    expected = ['lop', 'lop', 'ker', 'ker', 'mak', 'mak', 'vur', 'vur']
+    assert expected == p.stem(text)
+
+
+def test_stem_changing_consonants():
+    text = ['boffen', 'roven', 'vissen', 'glazen']
+    expected = ['bof', 'rof', 'vis', 'glas']
+    assert expected == p.stem(text)
+
+
+def test_stem__dimmunative_pje():
+    text = ['riempje', 'helmpje', 'museumpje']
+    expected = ['riem', 'helm', 'museum']
+    assert expected == p.stem(text)
+
+
+def test_stem_end():
+    text = ['groeiend', 'vermoeiend', 'verkopend']
+    expected = ['groei', 'vermoei', 'verkop']
+    assert expected == p.stem(text)
+
+
+def test_stem_past_irregular():
+    text = ['kocht', 'liep', 'mocht']
+    expected = ['kocht', 'liep', 'mocht']
+    assert expected == p.stem(text)
+
+
+def test_stem_past_regular():
+    text = ['deelde', 'werkten', 'smeedde', 'vergrootte']
+    expected = ['deel', 'werk', 'smed', 'vergrot']
+    assert expected == p.stem(text)
--- urbansearch/clustering/text_preprocessor.py
+++ urbansearch/clustering/text_preprocessor.py
@@ -4,14 +4,158 @@ import re
 from collections import Counter
 
 from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.snowball import SnowballStemmer, DutchStemmer
 from nltk.tokenize import word_tokenize
+
+
+class EnhancedDutchStemmer(DutchStemmer):
+    def __init__(self):
+        super().__init__()
+        self._double_consonants = (  # double consonants
+                                   'bb', 'cc', 'dd', 'ff', 'gg', 'kk', 'll',
+                                   'mm', 'nn', 'pp', 'rr', 'ss', 'tt', 'zz',
+                                   # for -t verbs
+                                   'bt', 'ct', 'dt', 'ft', 'gt', 'kt', 'lt',
+                                   'mt', 'pt', 'rt', 'st', 'zt'
+                                   # diminutives -je
+                                   'aj', 'bj', 'cj', 'dj', 'ej', 'fj', 'gj',
+                                   'hj', 'jj', 'kj', 'lj', 'mj', 'nj', 'pj',
+                                   'qj', 'rj', 'sj', 'tj', 'uj', 'vj', 'wj',
+                                   'xj', 'zj',
+                                   # for diminutives -*dje, * being a consonant
+                                   'bd', 'cd', 'dd', 'fd', 'gd', 'kd', 'ld',
+                                   'md', 'nd', 'pd', 'rd', 'sd', 'zd')
+
+    def stem_double_consonants(self, word):
+        """
+        The following is taken from step 4
+        from the DanishStemmer class (also NLTK)
+        It improves stems such as 'val' for verb 'vallen',
+        which would otherwise be stemmed as 'vall'.
+        This conflicts with third singular form 'valt',
+        which would be stemmed to 'val'.
+        In Dutch, there are many words behaving similarly.
+        :param word: A word that may contain a double equal consonant
+        :return: A word that does not have double consonants (**, *t, *j or *d)
+        """
+        for double_cons in self._double_consonants:
+            if word.endswith(double_cons) and 3 < len(word):
+                word = word[:-1]
+                break
+        return word
+
+    def stem_diminutives(self, word):
+        """
+        Changes words from diminutive form to root form.
+        :param word in a diminutive form (e.g. basje)
+        :return: word in a root form (e.g. bas)
+        """
+        exceptions = ['welletjes', 'innetjes', 'sprookje',
+                      'rotje', 'akkefietje', 'poffertje' 'vieruurtje']
+        if word in exceptions:
+            return word
+        if word in ['poffertjes', 'vieruurtjes']:      # plural exception
+            return word[:-1]
+
+        # word ends with -etje(s)
+        if word.endswith('etjes'):
+            if word.endswith('eetjes'):
+                return word[:-4]        # -tjes
+            else:
+                return word[:-5]        # -etjes
+        if word.endswith('etje'):
+            if word.endswith('eetje'):
+                return word[:-3]        # -tje
+            else:
+                return word[:-4]        # -etje
+
+        # word ends with '-inkje'
+        # This proves difficult.
+        # 62 words with ink, 2752 words with ing -> everything to ing
+        if word.endswith('inkje'):
+            word = word[:-3]
+            return word + 'g'
+        if word.endswith('inkjes'):
+            word = word[:-4]
+            return word + 'g'
+
+        # word ends with pje(s)
+        if word.endswith('pjes'):
+            return word[:-4]
+        if word.endswith('pje'):
+            return word[:-3]
+
+        # word ends with (t)je(s) and (t)je(s)
+        if word.endswith('jes'):    # -t gets removed in double consonant step
+            return word[:-3]
+        if word.endswith('je'):
+            return word[:-2]
+
+        return word
+
+    # method for past verbs?
+
+    def stem_changed_consonant(self, word):
+        """
+        some words when changed to plural change consonants,
+        this methods reverses that.
+        :param word: word in plural form (e.g. beven)
+        :return: word in root form (e.g. bef)
+        """
+        if len(word) > 4:
+            if word.endswith('oven') or word.endswith('aven') \
+                    or word.endswith('uven') or word.endswith('even'):
+                word = word[:-3] + 'f'
+        if len(word) > 4:
+            if word.endswith('azen') or word.endswith('ozen') \
+                    or word.endswith('uzen') or word.endswith('ezen'):
+                word = word[:-3] + "s"
+        return word
+
+    def stem(self, word):
+        """
+        Stems Dutch words
+        Included features:
+        - classic snowball
+        - double consonants (dt -> d)
+        - verbs where consonants changed (blozen -> blos)
+        - dimmunitives (slangetje -> slang)
+
+        known errors:
+        - words ending in kje that are derived from words
+          ending in nk are changed to ng ('vinkje' -> 'ving').
+        - double vowels are changed to single vowels ('gooi' -> 'goi').
+        - irregular (strong) verbs in past tense
+
+        :param word: The word to stem
+        :return: The stemmed word
+        """
+        word = self.stem_double_consonants(word)
+        word = self.stem_changed_consonant(word)
+
+        if word.startswith('groei'):
+            print(word)
+        word2 = self.stem_diminutives(word)
+        if word2 is word:
+            word2 = DutchStemmer.stem(self, word)
+
+        word2 = self.stem_double_consonants(word2)
+
+        return word2
 
 
 class PreProcessor:
     def __init__(self, lang='dutch'):
         self.lang = lang
         self.stemmer = None
+
+    def _get_stemmer(self):
+        if self.stemmer:
+            return self.stemmer
+        if self.lang == 'dutch':
+            return EnhancedDutchStemmer()
+        else:
+            return SnowballStemmer(self.lang)
 
     def pre_process(self, text):
         """
@@ -32,10 +176,7 @@ class PreProcessor:
         :param text: String list
         :return: String list
         """
-        if not self.stemmer:
-            self.stemmer = SnowballStemmer(self.lang)
-
-        return [self.stemmer.stem(w) for w in text]
+        return [self._get_stemmer().stem(w) for w in text]
 
     def strip_words(self, text):
         """
