da9df8a77cf7e2d510847883f07941284df1546f
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-05-15 16:06:31 +0200

    Replace tabs by spaces

--- urbansearch/clustering/decorators.py
+++ urbansearch/clustering/decorators.py
@@ -2,10 +2,11 @@ from functools import wraps
 
 
 def list_required(func):
-	@wraps(func)
-	def wrapper(self, doc, *args, **kwargs):
-		if isinstance(doc, list):
-			return func(self, doc, *args, **kwargs)
-		else:
-			raise TypeError('Invalid argument passed to %s' % func.__name__)
-	return wrapper
+    @wraps(func)
+    def wrapper(self, doc, *args, **kwargs):
+        if isinstance(doc, list):
+            return func(self, doc, *args, **kwargs)
+        else:
+            raise TypeError('Invalid argument passed to %s' % func.__name__)
+
+    return wrapper
