f593446034e0a3927c48395cc22820ba476b10f9
Marko Malis <you@example.com>
2017-05-18 10:03:42 +0200

    Add LDA functionality

--- urbansearch/clustering/relationextractor.py
+++ urbansearch/clustering/relationextractor.py
@@ -1,6 +1,7 @@
 from gensim import corpora, models
 from .decorators import list_required
 
+
 class RelationExtractor(object):
     def __init__(self, texts=None):
         """
@@ -10,6 +11,7 @@ class RelationExtractor(object):
         self.dictionary = corpora.Dictionary()
         self.tfidf_model = None
         self.lda_model = None
+        self.NUM_OF_TOPICS = 20
 
         if texts:
             self.extend_dictionary(texts, multiple=True)
@@ -44,7 +46,8 @@ class RelationExtractor(object):
         """
         if multiple:
             for text in doc:
-                self.extend_corpus(self.dictionary.doc2bow(text, allow_update=True))
+                self.extend_corpus(self.dictionary.doc2bow(text,
+                                                           allow_update=True))
         else:
             self.extend_corpus(self.dictionary.doc2bow(doc, allow_update=True))
 
@@ -56,6 +59,21 @@ class RelationExtractor(object):
         if self.tfidf_model:
             return self.tfidf_model[self.doc_to_bow(doc)]
 
+    @list_required
+    def extract_lda(self, doc):
+        """
+        TODO: documentation
+        """
+        if self.lda_model:
+            return self.lda_model[self.doc_to_bow(doc)]
+
+    def init_lda_model(self):
+        """
+        TODO: documentation and add functionality for LDA if desired
+        """
+        self.lda_model = models.LdaMulticore(self.corpus,
+                                             num_topics=self.NUM_OF_TOPICS)
+
     def init_tfidf_model(self):
         """
         TODO: documentation
@@ -64,12 +82,6 @@ class RelationExtractor(object):
             self.tfidf_model = models.TfidfModel(self.corpus)
 
         return self.tfidf_model
-
-    def init_lda_model(self):
-        """
-        TODO: documentation and add functionality for LDA if desired
-        """
-        pass
 
     def load_corpus(self, filename):
         """
@@ -83,6 +95,12 @@ class RelationExtractor(object):
         """
         self.dictionary.load(filename)
 
+    def load_lda(self, filename):
+        """
+        TODO: documentation
+        """
+        self.lda_model.load(filename)
+
     def save_corpus(self, filename):
         """
         TODO: documentation
@@ -94,6 +112,12 @@ class RelationExtractor(object):
         TODO: documentation
         """
         self.dictionary.save(filename)
+
+    def save_lda(self, filename):
+        """
+        TODO: documentation
+        """
+        self.lda_model.save(filename)
 
     # def update_corpus(self, corpus):
     #     """
