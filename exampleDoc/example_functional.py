809b9eae3c4ac34febfd04953f2c2c4c5950b6a5
ardwennem2 <tom_brunner@hotmail.com>
2017-05-18 13:56:05 +0200

    example functional and test file

---
+++ exampleDoc/example_functional.py
@@ -0,0 +1,61 @@
+import directimport
+
+from importlocation import importname
+
+environment_variable = environment_variable1
+
+
+def environment_function(foo, bar):
+    """
+    
+    :param foo: this is foo
+    :param bar: this is bar
+    return: return things
+    """
+
+
+class ExClass(foo)
+"""
+This class does stuff
+:param foo: this is foo
+"""
+    def __init__(self, bar=None):
+        if not bar:
+            bar = importname.method()
+
+    def class_fucntion(foo, bar):
+        """
+
+        :param foo: this is foo
+        :param bar: this is bar
+        return: return things
+        """
+
+    def class_fucntion2(foo, bar):
+        """
+
+        :param foo: this is foo
+        :param bar: this is bar
+        return: return things
+        """
+
+class ExClass2(foo)
+    def __init__(self, bar=None):
+        if not bar:
+            bar = directimport.method()
+
+    def class_fucntion(foo, bar):
+        """
+
+        :param foo: this is foo
+        :param bar: this is bar
+        return: return things
+        """
+
+    def class_fucntion2(foo, bar):
+        """
+
+        :param foo: this is foo
+        :param bar: this is bar
+        return: return things
+        """
---
+++ exampleDoc/example_test.py
@@ -0,0 +1,5 @@
+import importname
+
+from exampleDoc import example_functional_class
+
+f = example_functional_class()
