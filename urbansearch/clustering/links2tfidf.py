a425145bbd2fc8fa37d23ce0974ae7744f4cc2ec
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-05-19 14:25:00 +0200

    Pep8 fixes, cleanups

--- urbansearch/clustering/links2tfidf.py
+++ urbansearch/clustering/links2tfidf.py
@@ -1,25 +1,27 @@
-import config
 import os
-from link2doc import Link2Doc
-from relationextractor import RelationExtractor
+
+import config
+from .link2doc import Link2Doc
+from .relationextractor import RelationExtractor
 
 MODELS_DIRECTORY = config.get('resources', 'models')
 
+
 class Links2Tfidf(object):
-	def __init__(self, filename):
-		self.filename = os.path.join(MODELS_DIRECTORY, filename)
-		self.rex = RelationExtractor()
+    def __init__(self, filename):
+        self.filename = os.path.join(MODELS_DIRECTORY, filename)
+        self.rex = RelationExtractor()
 
-	def createModel(self):
-		l2d = Link2Doc()
+    def create_model(self):
+        l2d = Link2Doc()
 
-		with open(self.filename) as f:
-			for line in f:
-				print('********************')
-				print(line)
-				print('********************')
-				doc = l2d.get_doc(line)
-				rex.extend_dictionary(doc)
+        with open(self.filename) as f:
+            for line in f:
+                print('********************')
+                print(line)
+                print('********************')
+                doc = l2d.get_doc(line)
+                self.rex.extend_dictionary(doc)
 
-			self.rex.init_tfidf_model()
-			self.rex.tfidf_model.save(self.filename)
+            self.rex.init_tfidf_model()
+            self.rex.tfidf_model.save(self.filename)
