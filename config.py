3a102bca82ce890a23a55f6f5615f0c01943dea9
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-05-23 15:23:27 +0200

    Exclude setup.py and config.py and restore them

--- config.py
+++ config.py
@@ -8,104 +8,77 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(os.path.expanduser('~'), '.config', 'urbansearch')
 CONFIG_FILE = 'urbansearch.yml'
 
-_neo4j_config = {
-    'host': '',
-    'bolt_uri': '',
-    'username': '',
-    'password': '',
-}
-
-_resources_config = {
-    'test': os.path.join(BASE_DIR, 'tests', 'resources'),
-}
-
-_gathering_config = {
-    'cc_data': 'https://commoncrawl.s3.amazonaws.com/',
-    'cc_index': 'http://index.commoncrawl.org/',
-    'request_timeout': 2,
-}
-
-_gathering_logger = {
-    'handlers': ['file'],
-    'level': 'INFO',
-    'propagate': True,
-}
-
-_filtering_logger = {
-    'handlers': ['file'],
-    'level': 'INFO',
-    'propagate': True,
-}
-
-_clustering_logger = {
-    'handlers': ['file'],
-    'level': 'INFO',
-    'propagate': True,
-}
-
-_config_logger = {
-    'handlers': ['file'],
-    'level': 'DEBUG',
-    'propagate': True,
-}
-
-_urbansearch_logger = {
-    'handlers': ['file', 'console'],
-    'level': 'DEBUG',
-    'propagate': True,
-}
-
-_loggers = {
-    'urbansearch': _urbansearch_logger,
-    'config': _config_logger,
-    'clustering': _clustering_logger,
-    'filtering': _filtering_logger,
-    'gathering': _gathering_logger,
-}
-
-_file_handler = {
-    'level': 'DEBUG',
-    'class': 'logging.handlers.RotatingFileHandler',
-    'filename': os.path.join(BASE_DIR, 'urbansearch.log'),
-    'maxBytes': 10000000,
-    'backupCount': 5,
-    'formatter': 'default',
-}
-
-_console_handler = {
-    'level': 'WARN',
-    'class': 'logging.StreamHandler',
-    'formatter': 'default',
-}
-
-_handlers = {
-    'file': _file_handler,
-    'console': _console_handler
-}
-
-_formatters = {
-    'default': {
-        'format': '[%(levelname)s %(module)s] %(asctime)s || %(message)s',
-    }
-}
-
-_logging_config = {
-    'version': 1,
-    'formatters': _formatters,
-    'handlers': _handlers,
-    'loggers': _loggers,
-}
-
 # The configuration parameters.
 # They can all be overridden in the YAML config file.
 # For Neo4j, it is required to override the settings
 # since they have been left empty
 # for security purposes.
 CONFIG = {
-    'neo4j': _neo4j_config,
-    'resources': _resources_config,
-    'gathering': _gathering_config,
-    'logging': _logging_config,
+    'neo4j': {
+        'host': '',
+        'bolt_uri': '',
+        'username': '',
+        'password': '',
+    },
+    'resources': {
+        'test': os.path.join(BASE_DIR, 'tests', 'resources'),
+    },
+    'gathering': {
+        'cc_data': 'https://commoncrawl.s3.amazonaws.com/',
+        'cc_index': 'http://index.commoncrawl.org/',
+        'request_timeout': 2,
+    },
+    'logging': {
+        'version': 1,
+        'formatters': {
+            'default': {
+                'format': '[%(levelname)s %(module)s] %(asctime)s || %('
+                          'message)s',
+            }
+        },
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': os.path.join(BASE_DIR, 'urbansearch.log'),
+                'maxBytes': 10000000,
+                'backupCount': 5,
+                'formatter': 'default',
+            },
+            'console': {
+                'level': 'WARN',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+            }
+        },
+        'loggers': {
+            'urbansearch': {
+                'handlers': ['file', 'console'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            'config': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+            'clustering': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'filtering': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+            'gathering': {
+                'handlers': ['file'],
+                'level': 'INFO',
+                'propagate': True,
+            },
+        },
+    },
 }
 
 # Keep track of whether the system has been configured
