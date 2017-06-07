f5074be118839f37a9bf80eb47af9225e903ef7a
Gijs <gijsmw@hotmail.com>
2017-06-07 14:35:15 +0200

    Cleanup of plain text file downloader

--- urbansearch/gathering/text_downloader.py
+++ urbansearch/gathering/text_downloader.py
@@ -1,7 +1,9 @@
+import os
+from argparse import ArgumentParser
+from multiprocessing import Process
+
 from urbansearch.gathering import gathering, indices_selector
 from urbansearch.utils import process_utils
-from multiprocessing import Process
-import os
 
 
 class TextDownloader(object):
@@ -12,11 +14,12 @@ class TextDownloader(object):
 
     def run_workers(self, num_workers, directory, output_dir,  **kwargs):
         """ Run workers to process indices from a directory with files
-        in parallel. All parsed indices will be added to the queue.
+        in parallel. For each index the part will be downloaded and written
+        in plain text to the output directory.
 
         :num_workers: Number of workers that will run
-        :directory: Path to directory containing files
-        :queue: multiprocessing.Queue where the indices will be added to
+        :directory: Path to directory containing index files
+        :output_dir: Output directory for plain text files
         :gz: Passed in kwargs. Files are in .gz format. Default: True
         :opt: Passed in kwargs. Determine optimal number of workers and
         ignore num_workers parameter
@@ -28,8 +31,8 @@ class TextDownloader(object):
                  if _file.is_file()]
 
         div_files = process_utils.divide_files(files, num_workers)
-        workers = [Process(target=self.worker, args=(div_files[i], output_dir, i,
-                                                     kwargs.get('gz', True)))
+        workers = [Process(target=self.worker, args=(div_files[i], output_dir,
+                                                     i, kwargs.get('gz', True)))
                    for i in range(num_workers)]
 
         for worker in workers:
@@ -42,30 +45,49 @@ class TextDownloader(object):
     def worker(self, files, output_dir, w_id, gz=True):
         """
         Worker that will parse indices from files in file list and put the
-        results in a Queue. Can use plain text files containing indices or
-        .gz files containing indices.
+        write the results to separate files. Uses .gz files as input.
 
-        :queue: multiprocessing.JoinableQueue to put results in
         :files: List of filepaths to files that this worker will use
+        :output_dir: Output directory for the separate text files
+        :w_id: Id of this worker, to prevent writing to same file
         :gz: Use .gz files or not, default: True.
         """
+        rlv_indices = self.ind.relevant_indices_from_file
         if gz:
-            i = 0
             for file in files:
                 if file.endswith('.gz'):
-                    for index in self.ind.relevant_indices_from_file(file):
+                    for i, index in enumerate(rlv_indices(file)):
                         txt = self.pd.index_to_txt(index)
-                        self._write_txt_file_index(index, txt, output_dir, w_id, i)
-                        i += 1
+                        self._write_txt_file_index(index, txt, output_dir,
+                                                   w_id, i)
 
     def _write_txt_file_index(self, index, text, output_dir, w_id, n):
+        # Write index on first line of file, append the text
         final_text = str(index) + '\n' + text
-        with open(output_dir + "W{0}-{1}.txt".format(w_id, n), "w") as text_file:
-                text_file.write(final_text)
+        with open(output_dir + "W{0}-{1}.txt".format(w_id, n), "w") as f:
+                f.write(final_text)
+
+    def _parse_arguments():
+        parser = ArgumentParser(description='UrbanSearch TextDownloader')
+        parser.add_argument('directory',
+                            help='Source files directory containing files with'
+                                 + ' indices. .gz files only.')
+
+        parser.add_argument('workers',
+                            help='Number of parallel workers used')
+
+        parser.add_argument('output',
+                            help='Output directory where text containing at '
+                                 + 'least 1 co-occurrence will be stored.')
+        return parser.parse_args()
+
 
 if __name__ == "__main__":
     td = TextDownloader()
-    directory = '/home/gijs/BEP/test2/'
-    output_dir = '/home/gijs/BEP/pages/'
-    workers = 4 
+    args = td._parse_arguments()
+
+    directory = args.directory
+    output_dir = args.output
+    workers = args.workers
+
     td.run_workers(workers, directory, output_dir)
