90c96d0b5d288827d953caf76bdfa98ab4600d66
ardwennem2 <tom_brunner@hotmail.com>
2017-06-08 11:29:21 +0200

    cleanup code (pep8)

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,8 @@ from urbansearch import main
 
 @patch('urbansearch.main.Manager')
 @patch('urbansearch.gathering.indices_selector.IndicesSelector')
-def test_mock_selection_workers(mock_indices_selector, mock_manager):
+def test_mock_classify_documents_from_indices(mock_manager, mock_workers,
+                                              mock_indices_selector):
     with main.app.app_context():
         with patch('urbansearch.main.request') as mock_flask_request:
             ind_sel = mock_indices_selector.return_value = Mock()
@@ -15,7 +16,15 @@ def test_mock_selection_workers(mock_indices_selector, mock_manager):
             mock_flask_request.args.get('workers', 0).return_value = Mock()
             mock_flask_request.args.get('directory').return_value = Mock()
 
-            main.selection_workers()
+            producers = ind_sel.run_workers.return_value = [a, Mock()]
+            consumers = cworker.run_classifying_workers.return_value = \
+                [b, Mock()]
+
+            # Bugs other fixtures if imported globally.
+            from testfixtures import LogCapture
+            with LogCapture() as l:
+                main.classify_documents_from_indices()
+                assert ((l.__sizeof__()) > 0)
 
             assert mock_indices_selector.called
             assert mock_manager.called
