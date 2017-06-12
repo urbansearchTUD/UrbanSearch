d59bc005675c2ba81168a1c08be24e3062f95dea
Gijs <gijsmw@hotmail.com>
2017-06-12 15:18:43 +0200

    Some PEP8 and removed file

--- urbansearch/main.py
+++ urbansearch/main.py
@@ -150,27 +150,27 @@ def _parse_arguments():
     parser = ArgumentParser(description='The TU Delft Urbansearch CLI')
 
     parser.add_argument('-d', '--directory',
-                        help='Source files directory containing files with '
-                              + 'indices')
+                        help='Source files directory containing files with ' +
+                             'indices')
 
     parser.add_argument('-w', '--workers',
                         help='Number of parallel workers used')
 
     parser.add_argument('-c', '--config',
-                        help='Point to config file that will be used, config '
-                             + 'overrides default config')
+                        help='Point to config file that will be used, config' +
+                             ' overrides default config')
 
     parser.add_argument('-z', '--gzipped',
                         help='Only use .gz files available in directory')
 
     parser.add_argument('-o', '--output',
-                        help='Output directory where plain text containing at '
-                             + 'least 1 co-occurrence will be stored. By '
-                             + 'default the documents will not be stored.')
+                        help='Output directory where plain text containing ' +
+                             'at least 1 co-occurrence will be stored. By ' +
+                             'default the documents will not be stored.')
 
     parser.add_argument('-opt', '--optimal',
-                        help='Try to spawn optimal no. of  workers to put full'
-                             + ' load on the available CPU\'s')
+                        help='Try to spawn optimal no. of  workers to put' +
+                             ' load on the available CPU\'s')
 
     parser.add_argument('-v', '--verbose',
                         help='Verbose output, output all debug messages')
