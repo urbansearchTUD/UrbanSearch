e385c87fb847698f62a7050091fde16f856db745
Marko Malis <you@example.com>
2017-06-14 12:23:37 +0200

    Changed the indices route from the main program to an API call

--- tests/server/test_documents.py
+++ tests/server/test_documents.py
@@ -6,6 +6,7 @@ from urbansearch.server.main import Server
 CATEGORIES = config.get('score', 'categories')
 s = Server(run=False)
 
+
 def test_document_route():
     with s.app.test_client() as c:
         resp = c.get('/api/v1/documents')
