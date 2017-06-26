5c1cfdfb559ec414573ff7a1155f5f69d559a4f7
Piet van Agtmaal <P.J.C.vanAgtmaal@student.tudelft.nl>
2017-06-26 23:25:26 +0200

    Add classifier test

---
+++ tests/server/test_classifier.py
@@ -0,0 +1,113 @@
+from flask import json
+from unittest.mock import patch, Mock
+
+from urbansearch.gathering.indices_selector import IndicesSelector
+from urbansearch.server.main import Server
+from urbansearch.server import classifier
+from urbansearch.workers import Workers
+
+s = Server(run=False)
+
+@patch('urbansearch.server.classifier.dpu')
+@patch('urbansearch.server.classifier.smm')
+def test_train(smm, dpu):
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+    classifier.smm = Mock()
+
+    with s.app.test_client() as c:
+        resp = c.post('/api/v1/classifier/train?default=true')
+        assert classifier.dpu.generate_dataset.called
+        assert classifier.dpu.load.called
+        assert classifier.smm.train.called
+
+        assert json.loads(resp.data) == {'status': 200, 'message': 'success'}
+
+
+@patch('urbansearch.server.classifier.dpu')
+@patch('urbansearch.server.classifier.smm')
+def test_train_no_default(smm, dpu):
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+    classifier.smm = Mock()
+
+    with s.app.test_client() as c:
+        resp = c.post('/api/v1/classifier/train')
+        assert classifier.dpu.generate_dataset.called
+        assert classifier.dpu.load.called
+        assert classifier.smm.train.called
+
+        assert json.loads(resp.data) == {'status': 200, 'message': 'success'}
+
+
+@patch('urbansearch.server.classifier.SGDCModelManager')
+def test_train_equal(mm):
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+
+    with s.app.test_client() as c:
+        resp = c.post('/api/v1/classifier/train_equal?default=true')
+        assert classifier.dpu.generate_equal_dataset.called
+        assert classifier.dpu.load.called
+
+        assert json.loads(resp.data) == {'status': 200, 'message': 'success'}
+
+
+@patch('urbansearch.server.classifier.SGDCModelManager')
+def test_train_equal_no_default(mm):
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+
+    with s.app.test_client() as c:
+        resp = c.post('/api/v1/classifier/train_equal')
+        assert classifier.dpu.generate_equal_dataset.called
+        assert classifier.dpu.load.called
+
+        data = json.loads(resp.data)
+        assert data['status'] == 200
+        assert data['message'] == 'success'
+
+
+@patch('urbansearch.server.classifier.SGDCModelManager')
+def test_train_test_equal(mm):
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+    mm.score.return_value = 0
+
+    with s.app.test_client() as c:
+        resp = c.post('/api/v1/classifier/train_test_equal')
+        assert classifier.dpu.generate_equal_dataset.called
+        assert classifier.dpu.load.called
+
+
+@patch('urbansearch.server.classifier.SGDCModelManager')
+@patch('urbansearch.server.classifier.classification_report')
+def test_metrics_equal(cr, mm):
+    cr.return_value = 'success'
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+
+    with s.app.test_client() as c:
+        resp = c.get('/api/v1/classifier/metrics_equal')
+        assert classifier.dpu.generate_equal_dataset.called
+        assert classifier.dpu.load.called
+        
+        data = json.loads(resp.data)
+        assert data['status'] == 200
+        assert data['message'] == 'success'
+
+
+@patch('urbansearch.server.classifier.SGDCModelManager')
+@patch('urbansearch.server.classifier.classification_report')
+def test_probabilities_equal(cr, mm):
+    cr.return_value = 'success'
+    classifier.dpu = Mock()
+    classifier.dpu.load.return_value = {'inputs': [], 'outputs': []}
+
+    with s.app.test_client() as c:
+        resp = c.get('/api/v1/classifier/probabilities_equal')
+        assert classifier.dpu.generate_equal_dataset.called
+        assert classifier.dpu.load.called
+        
+        data = json.loads(resp.data)
+        assert data['status'] == 200
